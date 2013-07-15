ff09ee67dde8334106432348ebf39b1cc07f64ad
anjianshi <anjianshi@gmail.com>
2013-07-15 14:06:57 +0800

    优化 ExceptionHandler, update README

--- flask_api/__init__.py
+++ flask_api/__init__.py
@@ -8,25 +8,27 @@ from flask import make_response, json
 
 
 class APIManager(object):
-    app = None
+    def __init__(self):
+        self.app = None
 
-    # decorators that will be applied to API handler
-    # front weill be applied earlier
-    # 
-    # for performance, decorators will be applied when API handlers register,
-    # rather than when API handler be called.
-    # so change this property won't affect API handlers that already registered
-    #
-    # after all decorators be applied, the origin function will be returned (not the decorated one, just origin)
-    # so, you can call that function as well as it hasn't be decorated. no exception handle, no response format...
-    # and if you want a decorator be effictive all the time(regardless call like an API handler or normal function),
-    # you should add it manually, dont't add it in this decorators list
-    # example: auth decorator is the one should add manually
-    decorators = None
+        # decorators that will be applied to API handler
+        # front weill be applied earlier
+        # 
+        # for performance, decorators will be applied when API handlers register,
+        # rather than when API handler be called.
+        # so change this property won't affect API handlers that already registered
+        #
+        # after all decorators be applied, the origin function will be returned (not the decorated one, just origin)
+        # so, you can call that function as well as it hasn't be decorated. no exception handle, no response format...
+        # and if you want a decorator be effictive all the time(regardless call like an API handler or normal function),
+        # you should add it manually, dont't add it in this decorators list
+        # example: auth decorator is the one should add manually
+        self.decorators = None
 
-    # default decorators
-    exception_handler = None
-    response_formatter = None
+        # default decorators
+        self.exception_handler = None
+        self.response_formatter = None
+
 
     def init_app(self, app_or_blueprint):
         self.app = app_or_blueprint
@@ -61,34 +63,44 @@ class APIManager(object):
 # ==============================
 
 class ExceptionHandler(object):
-    def __init__(self):
-        self.registered_exceptions = [
-            # todo: add pre registered exceptions
-        ]
+    """the exception that has property "api_status", will be treat
+    "api_status" means http status, it's value like 404, 500, etc
+
+    if an exception need to be treat, but is not raised by API handler itself(eg. raised by 3rd library ), 
+    and you can't change it to have the "api_status" property, you can call "register_exception" method, 
+    register the exception into ExceptionHandler, then it will known how to handle it
+
+    after all, only exceptions that is the instance of Exception(or it's sub-class), will be treat
+    """
+
+    registered_exceptions = []
+
+    @classmethod
+    def register_exception(cls, *args, **kwargs):
+        cls.registered_exceptions.append(RegisteredException(*args, **kwargs))
 
     def __call__(self, f):
         def decorated_function(*args, **kwargs):
             try:
                 return f(*args, **kwargs)
             except Exception as e:
-                # notice: only exceptions that is the instance of the sub-class of Exception, will be handle
-                for registered_exception in self.registered_exceptions:
-                    if isinstance(e, registered_exception.exception_class):
-                        return make_response(registered_exception.message,
-                                             registered_exception.http_status)
+                if e.api_status:
+                    return make_response(e.message, e.api_status)
+                else:
+                    for registered_exception in self.registered_exceptions:
+                        if isinstance(e, registered_exception.exception_class):
+                            return make_response(registered_exception.message,
+                                                 registered_exception.api_status)
 
-                # if no registered_exception matched, raise the exception again
-                raise e
+                    # can't understand this exception, raise it again
+                    raise e
         return decorated_function
-
-    def register_exception(*args, **kwargs):
-        self.registered_exceptions.append(_RegisteredException(*args, **kwargs))
 
 
 class RegisteredException(object):
-    def __init__(self, exception_class, http_status=500, message=''):
+    def __init__(self, exception_class, api_status=500, message=''):
         self.exception_class = exception_class
-        self.http_status = http_status
+        self.api_status = api_status
         self.message = message
 
 
