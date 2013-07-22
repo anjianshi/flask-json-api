31b8d2b1886f59583d785aefe6ab3c3da16f4349
anjianshi <anjianshi@gmail.com>
2013-07-22 19:36:19 +0800

    增加 db model validator 功能

--- flask_json_api/db.py
+++ flask_json_api/db.py
@@ -3,10 +3,10 @@
 使用此模块需要 flask-sqlalchemy
 """
 
-__all__ = ['get_instance', 'model_conv']
+__all__ = ['get_instance', 'model_conv', 'validate_logic', 'validator']
 
 import flask.ext.sqlalchemy
-from sqlalchemy.orm import Query
+from sqlalchemy.orm import Query, validates as _orm_validates
 from sqlalchemy.util import KeyedTuple
 
 from conv import URLVarConverter
@@ -17,7 +17,12 @@ def get_instance(app):
     return flask.ext.sqlalchemy.SQLAlchemy(app)
 
 
-# ==================================
+# 此属性用来存放此模块用到的配置:
+#   'exclude_columns': []      调用 as_dict() 时默认要排除的 column
+flask.ext.sqlalchemy.Model.__api_args__ = None
+
+
+# ===== as_dict ====================
 
 def _model_as_dict(self, exclude=None, only=None):
     if exclude is None and only is None and self.__api_args__ is not None and 'exclude_columns' in self.__api_args__:
@@ -40,10 +45,6 @@ def _model_as_dict(self, exclude=None, only=None):
     return d
 flask.ext.sqlalchemy.Model.as_dict = _model_as_dict
 
-# 此属性用来存放此模块用到的配置:
-#   'exclude_columns': []      调用 as_dict() 时默认要排除的 column
-flask.ext.sqlalchemy.Model.__api_args__ = None
-
 
 def _query_as_dict(self, **kwargs):
     for row in self:
@@ -62,6 +63,36 @@ def _keyed_tuple_as_dict(self, **kwargs):
             result[key] = value
     return result
 KeyedTuple.as_dict = _keyed_tuple_as_dict
+
+
+# ===== simple validators ==========
+
+validate_logic = {
+    'min': lambda value, min: value is None or value >= min,
+    'max': lambda value, max: value is None or value <= max,
+    'min_length': lambda value, min: value is None or len(value) >= min,
+}
+
+
+def validator(columns, logic_name, *logic_args, **kwargs):
+    if not isinstance(columns, list):
+        columns = [columns]
+    logic = validate_logic[logic_name]
+
+    @_orm_validates(*columns, **kwargs)
+    def f(self, key, value):
+        if not logic(value, *logic_args):
+            args = []
+            for arg in logic_args:
+                args.append(str(arg))
+
+            raise BadRequest(
+                'db model validate failed: key={}, value={}, logic={}, arg={}'.format(
+                    key, value, logic_name, ','.join(args)))
+        return value
+
+    return f
+
 
 
 # ==================================
